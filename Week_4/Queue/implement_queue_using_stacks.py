e89e34da6adc571a8ec78f64ee7e915992f4bb55
Fahad Habib <fahad.habib@a006-00673.home>
2023-08-19 10:32:17 +0500

    Problem 5

---
+++ Week_4/Queue/implement_queue_using_stacks.py
@@ -0,0 +1,16 @@
+class MyQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue = [x] + self.queue
+
+    def pop(self) -> int:
+        return self.queue.pop()
+
+    def peek(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
