c884630bea9fd2082ff9e7fb7f7bf26dc3584ac8
Fahad-Habib <mr.fahadhabib@gmail.com>
2023-09-07 11:15:57 +0500

    Use Hash Table for happy_number

--- Week_7/happy_number.py
+++ Week_7/happy_number.py
@@ -1,8 +1,11 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        table = {}
         while True:
-            n = sum([int(i)**2 for i in str(n)])
-            if n == 37:
+            if n in table:
                 return False
+            table[n] = 0
+            table[n] = sum([int(i)**2 for i in str(n)])
+            n = sum([int(i)**2 for i in str(n)])
             if n == 1:
                 return True
