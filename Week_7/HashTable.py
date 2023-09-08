6e12cf6211780d731198cb546b93e1bbe78f7a58
Fahad-Habib <mr.fahadhabib@gmail.com>
2023-09-08 23:44:04 +0500

    Linear, Quadratic Probing

--- Week_7/HashTable.py
+++ Week_7/HashTable.py
@@ -1,4 +1,4 @@
-class Table:
+class HashTable:
 	def __init__(self):
 		self.length = 100
 		self.values = [None for _ in range(self.length)]
@@ -8,8 +8,59 @@ class Table:
 	
 	def __setitem__(self, key, value):
 		index = self.get_hash(key)
-		self.values[index] = value
+		self.values[index] = [key, value]
 	
 	def __getitem__(self, key):
 		index = self.get_hash(key)
-		return self.values[index]
+		return self.values[index][1]
+	
+
+class HashTableLinearProbing:
+	def __init__(self):
+		self.length = 100
+		self.values = [None for _ in range(self.length)]
+	
+	def get_hash(self, key):
+		index = key % self.length
+		while self.values[index]:
+			if self.values[index][0] == key:
+				return index
+			index = (index + 1) % self.length
+		return index
+
+	def __setitem__(self, key, value):
+		index = self.get_hash(key)
+		self.values[index] = [key, value]
+	
+	def __getitem__(self, key):
+		index = self.get_hash(key)
+		return self.values[index][1]
+
+
+class HashTableQuadraticProbing:
+	def __init__(self):
+		self.length = 100
+		self.values = [None for _ in range(self.length)]
+	
+	def get_hash(self, key):
+		index = key % self.length
+		if not self.values[index]:
+			return index
+		if self.values[index][0] == key:
+			return index
+		i = 1
+		new_index = (index + 1) % self.length
+		while self.values[new_index]:
+			if self.values[new_index][0] == key:
+				return new_index
+			i += 1
+			new_index = (index + (i**2)) % self.length
+		return new_index
+
+	def __setitem__(self, key, value):
+		index = self.get_hash(key)
+		self.values[index] = [key, value]
+	
+	def __getitem__(self, key):
+		index = self.get_hash(key)
+		return self.values[index][1]
